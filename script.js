b8abd8f26646efda65a13b183a69a235ab613e6e
Jared Siemen <siemenjm@gmail.com>
2020-10-15 12:29:14 -0400

    Add operate function to script.js, add buttons to html.

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ const multiply = function(a, b) {
 const divide = function(a, b) {
     return a / b;
 }
+
+const operate = function(operator, a, b) {
+    if (operator === "+") {
+        return add(a, b);
+    } else if (operator === "-") {
+        return subtract(a, b);
+    } else if (operator === "*") {
+        return multiply(a, b);
+    } else if (operator === "/") {
+        return divide(a, b);
+    }
+}
