e02a6130d9534111065dc8f4d5bb0f618b8fe785
Jared Siemen <siemenjm@gmail.com>
2020-10-15 16:56:54 -0400

    Make lots of changes with break in between. Got lost. Can now display numbers after clicking button.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Math Functions--------------------------------------------------------
+//-----------------------------------------------------------------------
 const add = function(a, b) {
     return a + b;
 }
@@ -25,3 +27,30 @@ const operate = function(operator, a, b) {
         return divide(a, b);
     }
 }
+
+// Query Selectors-------------------------------------------------------
+//-----------------------------------------------------------------------
+
+const memDisplay = document.querySelector("#memory");
+const display = document.querySelector("#display");
+const numberButtons = document.querySelectorAll(".number-btn");
+const operatorButtons = document.querySelectorAll(".operator-btn");
+
+// Event Listeners/Other Functions---------------------------------------
+//-----------------------------------------------------------------------
+
+const displayNumber = function(button) {
+    if (display.textContent === "0") {
+        display.textContent = button.value;
+    } else {
+        display.textContent += button.value;
+    }  
+};
+
+numberButtons.forEach(function(currentButton) {
+    currentButton.addEventListener("click", function() {displayNumber(currentButton)})
+});
+
+operatorButtons.forEach(function(currentButton) {
+    currentButton.addEventListener("click", function() {getOperator(currentButton)});
+});
